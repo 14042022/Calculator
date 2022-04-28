751bce294f5ae940ad44b3633fb16e649be3ee7e
#14042022 <104284240+14042022@users.noreply.github.com>
2022-04-27 23:49:02 -0300

    Update 1v - Added 4 basic arithmetic operations.

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ while True:
         print('Algoritmo de soma')
         a = int(input('Insira um valor: '))
         b = int(input('Insira outro valor: '))
-        resultado = a + b
-        print('O resultado é: '+str(resultado))
+        operacao = int(input('Que tipo de operação você quer?'
+                             '\n [1]Positivo        [2]Negativo'
+                             '\n [3]Multiplicação   [4]Divisão\n'))
+        if operacao == 1:
+            resultado = a + b
+            print('O resultado é: ' + str(resultado))
+        if operacao == 2:
+            resultado2 = a - b
+            print('O resultado é: '+str(resultado2))
+        if operacao == 3:
+            resultado3 = a * b
+            print('O resultado é: '+str(resultado3))
+        if operacao == 4:
+            resultado4 = a / b
+            print('O resultado é: '+str(resultado4))
+
     except ValueError:
         print ('Dado invalido')
         continue
